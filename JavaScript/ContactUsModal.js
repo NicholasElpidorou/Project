4948f25249cc41ae98db512a723d75453c813208
Nicholas Elpidorou <p14138568@my365.dmu.ac.uk>
2017-01-25 18:13:56 +0000

    No major changes fixed modals

--- JavaScript/ContactUsModal.js
+++ JavaScript/ContactUsModal.js
@@ -64,40 +64,4 @@ btnSubmit.onclick = function () {
 // window.onclick = function9even
 
 //
-// //ABOUT US MODAL
-//
-// Get the modal
-var modal2 = document.getElementById("AboutUsModal");
-
-// Get the button that opens the modal
-var btn2 = document.getElementById("AboutUs");
-
-// Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close")[0];
-//
-// //get the<button> element that user submits his form
-// var btnSubmit = document.getElementById("contact-submit");
-//
-// //get textFields element so user does not let them blank
-// var txtName = document.getElementById("Name");
-// var txtEmail = document.getElementById("Email");
-// var txtDescription = document.getElementById("Description");
-
-
-// When the user clicks the button, open the modal
-btn2.onclick = function() {
-    modal2.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span2.onclick = function() {
-    modal2.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal2) {
-        modal2.style.display = "none";
-    }
-}
 
---
+++ JavaScript/RateUsModal.js
@@ -0,0 +1,3 @@
+/**
+ * Created by NicholasElpidorou on 25/01/2017.
+ */
