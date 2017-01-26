a24743283d38e8e219ae946e33ffbb1b2b349447
Nicholas Elpidorou <p14138568@my365.dmu.ac.uk>
2017-01-26 02:48:19 +0000

    created a modal for products

--- JavaScript/ProductChooserButtons.js
+++ JavaScript/ProductChooserButtons.js
@@ -15,11 +15,11 @@ function ShowMen() {
     document.getElementById("products").style.display='block';
 
     document.getElementById("img1").src = "../Images/ProductImages/Men/MenWatch.jpeg"
-    document.getElementById("link1").href = "ProductPages/Men/Watches.html"
+    // document.getElementById("link1").href = "ProductPages/Men/Watches.html"
     document.getElementById("text1").innerHTML = 'Watches'
 
     document.getElementById("img2").src = "../Images/ProductImages/Men/MenBracelet.jpg"
-    document.getElementById("link2").href = "ProductPages/Men/Bracelets.html"
+    // document.getElementById("link2").href = "ProductPages/Men/Bracelets.html"
     document.getElementById("text2").innerHTML = 'Bracelets'
 
     document.getElementById("MenLink").style.backgroundColor = 'black';
@@ -35,11 +35,11 @@ function ShowWomen() {
     document.getElementById("products").style.display='block';
 
     document.getElementById("img1").src = "../Images/ProductImages/Women/WomanNecklace.jpeg"
-    document.getElementById("link1").href = "ProductPages/Women/Necklaces.html"
+    // document.getElementById("link1").href = "ProductPages/Women/Necklaces.html"
     document.getElementById("text1").innerHTML = 'Necklace'
 
     document.getElementById("img2").src = "../Images/ProductImages/Women/WomanRing.jpg"
-    document.getElementById("link2").href = "ProductPages/Women/Rings.html"
+    // document.getElementById("link2").href = "ProductPages/Women/Rings.html"
     document.getElementById("text2").innerHTML = 'Rings'
 
     document.getElementById("MenLink").style.backgroundColor = 'transparent';
---
+++ JavaScript/ProductsModal.js
@@ -0,0 +1,41 @@
+/**
+ * Created by NicholasElpidorou on 24/01/2017.
+ */
+
+//CONTACT US MODAL
+
+// Get the modal
+var modal = document.getElementById("ProductModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("link1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//get the<button> element that user submits his form
+var btnSubmit = document.getElementById("contact-submit");
+
+//get textFields element so user does not let them blank
+var txtName = document.getElementById("Name");
+var txtEmail = document.getElementById("Email");
+var txtDescription = document.getElementById("Description");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
