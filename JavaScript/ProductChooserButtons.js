d74fe8078b973124c7bc9784b508c0c9d9b2505c
Nicholas Elpidorou <p14138568@my365.dmu.ac.uk>
2017-01-26 02:20:32 +0000

    commiting before creating a branch...Will start experimenting on how to make product pages

--- JavaScript/ProductChooserButtons.js
+++ JavaScript/ProductChooserButtons.js
@@ -12,89 +12,61 @@
 
 function ShowMen() {
 
-    document.getElementById("products").style.accelerator
     document.getElementById("products").style.display='block';
 
     document.getElementById("img1").src = "../Images/ProductImages/Men/MenWatch.jpeg"
+    document.getElementById("link1").href = "ProductPages/Men/Watches.html"
     document.getElementById("text1").innerHTML = 'Watches'
 
-    document.getElementById("img2").src = "../Images/ProductImages/Men/MenRing.jpg"
-    document.getElementById("text2").innerHTML = 'Ring'
+    document.getElementById("img2").src = "../Images/ProductImages/Men/MenBracelet.jpg"
+    document.getElementById("link2").href = "ProductPages/Men/Bracelets.html"
+    document.getElementById("text2").innerHTML = 'Bracelets'
 
-    document.getElementById("img3").src = "../Images/ProductImages/Men/MenBracelet.jpg"
-    document.getElementById("text3").innerHTML = 'Bracelets'
-
-    document.getElementById("img4").src = "../Images/ProductImages/Men/MenWallet.jpg"
-    document.getElementById("text4").innerHTML = 'Wallet'
-
-    document.getElementById("img5").src = "../Images/ProductImages/Men/MenEarings.jpg"
-    document.getElementById("text5").innerHTML = 'Earings'
-
-    document.getElementById("img6").src = "../Images/ProductImages/Men/MenNecklaces.jpg"
-    document.getElementById("text6").innerHTML = 'Necklaces'
-
-    document.getElementById("BtnMen").disabled = true
-    document.getElementById("BtnWomen").disabled = false
-    document.getElementById("BtnAccessories").disabled = false
-
-
-
-
-
+    document.getElementById("MenLink").style.backgroundColor = 'black';
+    document.getElementById("WomenLink").style.backgroundColor = 'transparent';
+    document.getElementById("AccessoriesLink").style.backgroundColor = 'transparent';
 }
 
 function ShowWomen() {
+
+    // var link1 = document.getElementById("link1");
+    // var link2 = document.getElementById("link2");
+
     document.getElementById("products").style.display='block';
 
-    document.getElementById("img1").src = "../Images/ProductImages/Women/WomanBracelet.jpg"
-    document.getElementById("text1").innerHTML = 'Bracelets'
+    document.getElementById("img1").src = "../Images/ProductImages/Women/WomanNecklace.jpeg"
+    document.getElementById("link1").href = "ProductPages/Women/Necklaces.html"
+    document.getElementById("text1").innerHTML = 'Necklace'
 
-    document.getElementById("img2").src = "../Images/ProductImages/Women/WomanEaring.jpg"
-    document.getElementById("text2").innerHTML = 'Earings'
+    document.getElementById("img2").src = "../Images/ProductImages/Women/WomanRing.jpg"
+    document.getElementById("link2").href = "ProductPages/Women/Rings.html"
+    document.getElementById("text2").innerHTML = 'Rings'
 
-    document.getElementById("img3").src = "../Images/ProductImages/Women/WomanNecklace.jpeg"
-    document.getElementById("text3").innerHTML = 'Necklaces'
+    document.getElementById("MenLink").style.backgroundColor = 'transparent';
+    document.getElementById("WomenLink").style.backgroundColor = 'black';
+    document.getElementById("AccessoriesLink").style.backgroundColor = 'transparent';
 
-    document.getElementById("img4").src = "../Images/ProductImages/Women/WomanRing.jpg"
-    document.getElementById("text4").innerHTML = 'Rings'
-
-    document.getElementById("img5").src ="../Images/ProductImages/Women/WomanWatch.jpg"
-    document.getElementById("text5").innerHTML = 'Watches'
-
-    document.getElementById("img6").src = "../Images/ProductImages/Women/Cuffs.jpg"
-    document.getElementById("text6").innerHTML = 'Cuffs'
-
-    document.getElementById("BtnMen").disabled = false
-    document.getElementById("BtnWomen").disabled = true
-    document.getElementById("BtnAccessories").disabled = false
-
-
+    // window.open(link1.href,'_blank');
+    // // window.open(link2.href,'_blank');
+    //
+    // link.innerHTML = "facebook"
+    // link1.setAttribute('href',"http://facebook.com");
+    // return false;
 }
 
 function ShowAccessories() {
     document.getElementById("products").style.display='block';
 
     document.getElementById("img1").src = "../Images/ProductImages/Accessories/KeyRing.jpg"
+    document.getElementById("link1").href = "ProductPages/Accessories/KeyRings.html"
     document.getElementById("text1").innerHTML = 'Key Rings'
 
     document.getElementById("img2").src = "../Images/ProductImages/Accessories/Zippo.jpg"
+    document.getElementById("link2").href = "ProductPages/Accessories/Zippos.html"
+
     document.getElementById("text2").innerHTML = 'Zippo'
 
-    document.getElementById("img3").src = "../Images/ProductImages/Accessories/JeweleryBox.jpg"
-    document.getElementById("text3").innerHTML = 'Jewelery boxes'
-
-    document.getElementById("img4").src = "../Images/ProductImages/Accessories/Decorations.jpg"
-    document.getElementById("text4").innerHTML = 'Decorations'
-
-    document.getElementById("img5").src = "../Images/ProductImages/Accessories/Pen.jpg"
-    document.getElementById("text5").innerHTML = 'Pens'
-
-    document.getElementById("img6").src = "../Images/ProductImages/Accessories/PhoneCase.jpg"
-    document.getElementById("text6").innerHTML = 'Phone cases'
-
-    document.getElementById("BtnMen").disabled = false
-    document.getElementById("BtnWomen").disabled = false
-    document.getElementById("BtnAccessories").disabled = true
-
-
+    document.getElementById("MenLink").style.backgroundColor = 'transparent';
+    document.getElementById("WomenLink").style.backgroundColor = 'transparent';
+    document.getElementById("AccessoriesLink").style.backgroundColor = 'black';
 }
